833c86aa34931b0cf3a242b55460bb280d657b42
Marcin Galiński <marcin.galinski95@gmail.com>
2015-11-23 20:00:29 +0100

    implement my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -7,4 +7,12 @@ module Enumerable
 	 		for index in 0...self.length do yield(keys[index], self[keys[index]]); end
 	 	end		
 	end
+	def my_each_with_index
+		if self.is_a? Array
+	 		for index in 0...self.length do yield(self[index], index); end 
+	 	elsif self.is_a? Hash
+	 		keys = self.keys
+	 		for index in 0...self.length do yield(keys[index], self[keys[index]], index); end
+	 	end	
+	end
 end
