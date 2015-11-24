1b349b9c3539403e99ed75b5c1e5df388e1675cb
Marcin Galiński <marcin.galinski95@gmail.com>
2015-11-24 20:03:12 +0100

    implement my_count method

--- enumerables.rb
+++ enumerables.rb
@@ -1,44 +1,52 @@
 module Enumerable
 	def my_each
-	 	if self.is_a? Array
-	 		for index in 0...self.length do yield(self[index]); end 
-	 	elsif self.is_a? Hash
-	 		keys = self.keys
-	 		for index in 0...self.length do yield(keys[index], self[keys[index]]); end
-	 	end		
+	 	for index in 0...self.length do yield(self[index]); end 
 	end
+
 	def my_each_with_index
-		if self.is_a? Array
-	 		for index in 0...self.length do yield(self[index], index); end 
-	 	elsif self.is_a? Hash
-	 		keys = self.keys
-	 		for index in 0...self.length do yield(keys[index], self[keys[index]], index); end
-	 	end	
+	 	for index in 0...self.length do yield(self[index], index); end 
 	end
+
 	def my_select
-		array = Array.new
-		self.my_each do |*key, value|
-			array << value if yield(*key, value)
+		result_array = Array.new
+		self.my_each do |value|
+			result_array << value if yield value
 		end
-		array
+		result_array
 	end
+
 	def my_all?
 		result = true
-		self.my_each do |*key, value|
-			result = false unless yield(*key, value)
+		self.my_each do |value|
+			result = false unless yield value
 		end
 		result
 	end
+
 	def my_any?
 		result = false
-		self.my_each do |*key, value|
-			result = true if yield(*key, value)
+		self.my_each do |value|
+			result = true if yield value
 		end
 		result
 	end
+
 	def my_none?
-		!self.my_any? { |*key, value| yield(*key, value) }
+		!self.my_any? { |value| yield value }
 	end
+
+	def my_count argument=nil
+		count = 0
+		if block_given?
+			self.my_each { |value| count += 1 if yield(value) }
+		elsif argument == nil
+			self.my_each { |value| count += 1 }
+		else
+			self.my_each { |value| count += 1 if value == argument }
+		end
+		count
+	end
+
 	def my_map
 		result = []
 		self.my_each { |*key, value| result << yield(*key, value) }
