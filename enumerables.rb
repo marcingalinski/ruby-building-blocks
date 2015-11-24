80c4f0c315eceabcfe6fce427899a5189adc1bbd
Marcin Galiński <marcin.galinski95@gmail.com>
2015-11-24 21:46:21 +0100

    implement my_inject method

--- enumerables.rb
+++ enumerables.rb
@@ -9,25 +9,31 @@ module Enumerable
 
 	def my_select
 		result_array = Array.new
+
 		self.my_each do |value|
 			result_array << value if yield value
 		end
+
 		result_array
 	end
 
 	def my_all?
 		result = true
+
 		self.my_each do |value|
 			result = false unless yield value
 		end
+
 		result
 	end
 
 	def my_any?
 		result = false
+
 		self.my_each do |value|
 			result = true if yield value
 		end
+
 		result
 	end
 
@@ -37,6 +43,7 @@ module Enumerable
 
 	def my_count argument=nil
 		count = 0
+
 		if block_given?
 			self.my_each { |value| count += 1 if yield(value) }
 		elsif argument == nil
@@ -44,12 +51,27 @@ module Enumerable
 		else
 			self.my_each { |value| count += 1 if value == argument }
 		end
+
 		count
 	end
 
 	def my_map
-		result = []
+		result_array = Array.new
 		self.my_each { |*key, value| result << yield(*key, value) }
-		result
+		result_array
+	end
+
+	def my_inject *args
+		symbol = (args.my_select { |arg| arg.is_a? Symbol })[0]
+		memo = (args.my_select { |arg| !arg.is_a? Symbol })[0]
+		memo ||= self[0]
+
+		if block_given?
+			self.my_each { |value| memo = yield(memo, value) }
+		else
+			self.my_each { |value| memo = memo.send(symbol, value) }
+		end
+
+		memo
 	end
 end
