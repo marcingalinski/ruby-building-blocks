3f0a32c5b9ec0b428f1c3b70dbce658abfd9f568
Marcin Galiński <marcin.galinski95@gmail.com>
2015-11-24 21:46:45 +0100

    refactor code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,27 +1,30 @@
 class Array
 	def bubble_sort
 		sort_it(self, (self[index] > self[index + 1]))
-		self
 	end
 
 	def bubble_sort_by
 		sort_it(self, (yield(self[index], self[index + 1]) > 0))
-		self
 	end
 
 	private
-	def sort_it(array, condition)
+	def sort_it(array, swapped, condition)
 		swapped = true
-		while swapped do
+		while swapped do 
 			swapped = false
-			for index in 0...(array.length - 1)
-				if condition
-					value = array[index]
-					array[index] = array[index + 1]
-					array[index + 1] = value
-					swapped = true
-				end
-			end
+			iterate_array(array, swapped, condition) 
 		end
+		array
+	end
+
+	def iterate_array(array, swapped, condition)
+		for index in 0...(array.length - 1) { swap(array, swapped) if condition }
+	end
+
+	def swap(array, swapped)
+		value = array[index]
+		array[index] = array[index + 1]
+		array[index + 1] = value
+		swapped = true
 	end
 end
