470ff48665c3f8763365658c34cee4326fb51b81
Marcin Galiński <marcin.galinski95@gmail.com>
2016-01-08 20:45:49 +0100

    refactoring caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,25 +1,14 @@
-def convert (text, key)
-	text_output = ""
+def convert(text, key)
+	downcase = ('a'..'z').to_a
+	upcase = ("A".."Z").to_a
 
-	alphabet_downcase = Array.new
-	for letter in ("a".."z") do
-		alphabet_downcase << letter
-	end
-
-	alphabet_upcase = Array.new
-	for letter in ("A".."Z") do
-		alphabet_upcase << letter
-	end
-
-	text.each_char do |char|
-		if alphabet_downcase.include? char
-			text_output << alphabet_downcase[((alphabet_downcase.index(char) + key) % alphabet_downcase.length)]
-		elsif alphabet_upcase.include? char
-			text_output << alphabet_upcase[((alphabet_upcase.index(char) + key) % alphabet_upcase.length)]
+	text.each_char.map do |char|
+		if downcase.include? char
+			downcase[((downcase.index(char) + key) % downcase.length)]
+		elsif upcase.include? char
+			upcase[((upcase.index(char) + key) % upcase.length)]
 		else
-			text_output << char
+			char
 		end
 	end
-
-	puts text_output
 end
